ca7ae9e5fd9e29e8be65c255bd8219fe0c05fd98
luuvanhung <lvhdev99@gmail.com>
2021-07-08 23:18:40 +0700

    update code first

---
+++ index.js
@@ -0,0 +1,54 @@
+// const username = prompt("Who is there?", "");
+
+// if (username === "Admin") {
+//   const password = prompt("Password?", "");
+//   if (password === "Hung") {
+//     alert("Welcome Hung");
+//     const want = prompt("Do you want to? ", "");
+//     if(want === "watch") {
+//         alert("OK, thanks!");
+//     }
+//     else {
+//         alert("Canceled")
+//     }
+//   } else if (password === "" || password === null) {
+//     alert("Canceled");
+//   } else {
+//     alert("Wrong password");
+//   }
+// } else if (username === "" || username === null) {
+//   alert("Canceled");
+// } else {
+//   alert("I don't know you");
+// }
+
+// let sum = 0;
+
+// while (true) {
+
+//   let value = +prompt("Enter a number", '');
+
+//   if (!value) break; // (*)
+
+//   sum += value;
+
+// }
+// alert( 'Sum: ' + sum );
+// let i = 3;
+
+// while (i) {
+//   alert( i-- );
+// }
+
+// const result = () => {}
+
+let sum = 10;
+
+function hello () {
+    let sum = 20;
+    total=0;
+    total += sum;
+
+}
+alert(hello);
+
